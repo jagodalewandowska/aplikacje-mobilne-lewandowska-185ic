51e0246a0789bbea176a6c4028bf36572d1ae460
Jagoda Lewandowska <jadlewandowska@gmail.com>
2021-04-18 16:09:49 +0200

    TextInput done

--- Lab4/components/TextIn.js
+++ Lab4/components/TextIn.js
@@ -1,31 +1,121 @@
-import React, { useState, useEffect }  from 'react';
-import { TouchableOpacity, View, Text, TextInput } from 'react-native';
+import React, { Component }  from 'react';
+import { TouchableOpacity, View, Text, TextInput, Modal, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 
-export default function TextIn({navigation}) {  
-  return (
-    <View style={styles.con.container}> 
-      {/* Nawigacja */}
-      <View style={styles.con.nav}>        
-        <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('Select')}>
-            <Text style={styles.con.text}>Select</Text>
-        </TouchableOpacity>      
-        <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('Switch')}>
-            <Text style={styles.con.text}>Switch</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.con.home} onPress={() => navigation.push('Strona Główna')}>
-            <Text style={styles.con.text}>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('DatePicker')}>
-            <Text style={styles.con.text}>DatePicker</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('ToastAndroid')}>
-            <Text style={styles.con.text}>Toast</Text>
-        </TouchableOpacity>
-      </View>   
-      <View style={{flex: 1}}>
-        
-      </View>    
-    </View>      
-  );
+export default function(props) {
+  const navigation = useNavigation();
+  return <TextIn {...props} navigation = {navigation} />;
+}
+
+export class TextIn extends Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      colour: "",
+      multiline: "TextInput is a foundational component for inputting text into the app via a keyboard. Props provide configurability for several features, such as auto-correction, auto-capitalization, placeholder text, and different keyboard types, such as a numeric keypad.",
+      editable: false
+    }
+    this.handleEditable = this.handleEditable.bind(this)
+  }
+
+  // Przykład 1
+  handleName = (text) => {
+    this.setState({ name: text })
+  }    
+  handleColour = (text) => {
+    this.setState({ colour: text })
+  }
+  handleAlert(name, colour){
+    alert("Witaj " + name + ", Twój ulubiony kolor to " + colour + ".");
+  }
+
+  // Przykład 2
+  handleMultiline = (text) => {
+    this.setState({ multiline: text })
+  }
+  calcInput(text){
+    alert('Ilość znaków: ' + text.length + ".")
+  }
+
+  // Przykład 3
+  handleEditable() {
+    this.setState({
+      editable: !this.state.editable,
+    })
+  }
+
+  render(){
+    const { navigation } = this.props;    
+    return (
+      <View style={styles.con.container}> 
+        {/* Nawigacja */}
+        <View style={styles.con.nav}>        
+          <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('Select')}>
+              <Text style={styles.con.text}>Select</Text>
+          </TouchableOpacity>      
+          <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('Switch')}>
+              <Text style={styles.con.text}>Switch</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.con.home} onPress={() => navigation.push('Strona Główna')}>
+              <Text style={styles.con.text}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('DatePicker')}>
+              <Text style={styles.con.text}>DatePicker</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.con.button} onPress={() => navigation.push('ToastAndroid')}>
+              <Text style={styles.con.text}>Toast</Text>
+          </TouchableOpacity>
+        </View>   
+        <View style={styles.body.container}>
+          <ScrollView>
+            <View style={styles.body.ex}>
+              <Text style={styles.body.txt}>1. Przekazywanie parametrów do komunikatu.</Text>
+                <View style={styles.body.byside}>
+                  <TextInput style = {styles.body.input}
+                    placeholder = "Imię"
+                    onChangeText = {this.handleName}
+                  />
+                  <TextInput style = {styles.body.input}
+                    placeholder = "Ulubiony kolor"
+                    onChangeText = {this.handleColour}
+                  />
+                </View> 
+                <TouchableOpacity style={styles.body.btn} onPress = { () => {this.handleAlert(this.state.name, this.state.colour)}}>
+                  <Text style={styles.home.text}>Wyślij</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.body.ex}>
+              <Text style={styles.body.txt}>2. TextInput z wieloma liniami, wpisanym wcześniej tekstem i maksymalną ilością znaków.</Text>
+              <View style={styles.body.byside}>
+                <TextInput style = {styles.body.input2}                
+                  multiline = {true}
+                  numberOfLines={3}
+                  value= {this.state.multiline}
+                  onChangeText = {this.handleMultiline}
+                  maxLength = {this.maxLength}
+                />
+              </View> 
+              <TouchableOpacity style={styles.body.btn} onPress = { () => {this.calcInput(this.state.multiline)}}>
+                <Text style={styles.home.text}>Zlicz znaki</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.body.ex}>
+              <Text style={styles.body.txt}>3. Zablokowanie input po wciśnięciu przycisku</Text>
+              <View style={styles.body.byside}>
+                <TextInput style = {styles.body.input2}                
+                  placeholder = "Wpisz coś"
+                  editable = {this.state.editable}
+                />
+              </View> 
+              <TouchableOpacity style={styles.body.btn} onPress = {this.handleEditable}>
+                <Text style={styles.home.text}>Edytuj</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>    
+      </View>      
+    );
+  } 
 }
--- Lab4/components/styles.js
+++ Lab4/components/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = {}
 
-// ------------ Nawigacja ------------ //
+// ------------ Nawigacja - Home ------------ //
 
 styles.home = StyleSheet.create({
     nav: {
@@ -29,6 +29,8 @@ styles.home = StyleSheet.create({
         height: 255,
     }
 });
+
+// ----------- Nawigacja - Screens ----------- //
 
 styles.con = StyleSheet.create({
     container: {
@@ -63,6 +65,51 @@ styles.con = StyleSheet.create({
         borderRadius: 15,
         paddingVertical: 7,
         paddingHorizontal: 12,
+    }
+});
+
+// ----------- Zawartość ----------- //
+
+styles.body = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    ex: {        
+        flexDirection: 'column',
+        margin: '2%'
+    },
+    txt: {        
+        fontWeight: 'bold',
+        fontSize: 15,
+        margin: '1%',  
+    },
+    input: {
+        margin: 5,
+        height: 35,
+        borderColor: '#006164',
+        borderWidth: 1,
+        padding: '3%',
+        fontSize: 13,
+        width: '49%'       
+    },
+    input2: {
+        margin: 5,
+        borderColor: '#006164',
+        borderWidth: 1,
+        padding: '3%',
+        fontSize: 13,
+        width: '100%'
+    },
+    byside: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    btn: {
+        marginTop: 10,
+        elevation: 6,
+        backgroundColor: "#006164",
+        paddingVertical: 10
     }
 });
 
